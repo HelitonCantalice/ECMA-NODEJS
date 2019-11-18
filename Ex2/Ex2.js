b23b637ec99a3c1fbe70bb558f77291899c30de2
Heliton Pereira Cantalice Junior <47894352+HelitonCantalice@users.noreply.github.com>
2019-11-18 10:48:18 -0300

    Initial commit

---
+++ Ex2/Ex2.js
@@ -0,0 +1,11 @@
+const usuarios = [
+    {nome : 'Diego', idade: 23, empresa : 'RockeatSeat'},
+    {nome : 'Gabriel', idade : 15, empresa : 'RockeatSeat' },
+    {nome : 'Lucas', idade : 30, empresa : 'Facebook'},
+
+];
+
+// MAP 
+
+const idades = usuario.map(usuarios => usuarios.idade);
+console.log(idades);
